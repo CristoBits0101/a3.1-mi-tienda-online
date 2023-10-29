0f249ec5747bfa4fa4994f2354dba5e520b95198
Cristo RPS <cristobits0101@gmail.com>
2023-10-29 12:06:58 +0000

    Añadiendo el script listado_productos

---
+++ listado_productos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Consultar el listado de productos</title>
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+        <!-- Almacena la aplicación -->
+        <div class="container">
+
+            <!-- Incluye el menú dentro de la aplicación -->
+            <?php
+                include_once "./index.php";
+            ?>
+
+            <!-- Aquí va el cuerpo del contenido -->
+            <main>
+
+               
+            </main>
+        </div>
+    </body>
+</html>
