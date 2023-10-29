9c2948f83acdcd0c06bacc040aacffa8dccef86f
Cristo RPS <cristobits0101@gmail.com>
2023-10-29 12:56:51 +0000

    Actualizando tabla

--- listado_productos.php
+++ listado_productos.php
@@ -1,3 +1,7 @@
+<?php
+    // Paso 1) Importamos el archivo de configuración para poder conectarnos a la base de datos.
+    require_once "./configuration.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,9 +25,69 @@
             ?>
 
             <!-- Aquí va el cuerpo del contenido -->
-            <main>
+            <main id="listado-productos">
 
-               
+                <!-- Creamos la tabla que va a almacenar la consulta -->
+                <table>
+
+                    <!-- Asignamos un título a la tabla -->
+                    <caption>Listado de Productos</caption>
+
+                    <!-- Añadimos el encabezado estático de la tabla -->
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Imagen</th>
+                            <th>Categoría</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+
+                    <!-- Añadimos el cuerpo de la tabla de forma dinámica -->
+                    <tbody>
+                        <?php
+
+                            // Establece la conexión a la base de datos.
+                            $conn = connect_to_database();
+
+                            // Realiza una consulta SQL para obtener los datos de la tabla 'productos'.
+                            $sql = "SELECT * FROM productos";
+                            $stmt = $conn->query($sql);
+
+                            if ($stmt) 
+                            {
+                                while ($row = $stmt->fetch()) 
+                                {
+                                    echo '<tr>';
+                                        echo '<td>' . $row['Nombre'] . '</td>';
+                                        echo '<td>' . $row['Precio'] . '</td>';
+                                        echo '<td>
+                                                <img src=".\\ficheros\\' . $row['Imagen'] . '" alt="Imagen del producto" width="100" />
+                                            </td>';
+                                        echo '<td>' . $row['Categoría'] . '</td>';
+                                        echo '<td>
+                                                <a href="edita_producto.php?id=' . $row['id'] . '">
+                                                    <img src="" alt="Editar" />
+                                                </a>
+                                            </td>';
+                                        echo '<td>
+                                                <a href="elimina_producto.php?id=' . $row['id'] . '">
+                                                    <img src="" alt="Eliminar">
+                                                </a>
+                                            </td>';
+                                    echo '</tr>';
+                                }
+                            }
+                            
+                            else
+                            {
+                                echo '<tr><td colspan="4">No se pudieron recuperar los datos.</td></tr>';
+                            }
+                        ?>
+                    </tbody>
+                </table>
             </main>
         </div>
     </body>
